5e3969a0584f979cada0ebbbbaa5140815b611b8
Thomas Price <pablo@cbrz.com>
2024-12-09 21:59:28 -0500

    (file watcher) added struct for Line to be passed to Parser

--- internal/filewatcher/watcher.go
+++ internal/filewatcher/watcher.go
@@ -13,13 +13,19 @@ type FileWatcher struct {
 	writer   chan string
 }
 
+type Line struct {
+	Content   string
+	Source    string
+	Timestamp time.Time
+}
+
 func NewFileWatcher(filename string) (*FileWatcher, error) {
 	return &FileWatcher{
 		filename: filename,
 	}, nil
 }
 
-func (fw *FileWatcher) Watch(writer chan<- string) error {
+func (fw *FileWatcher) Watch(writer chan<- *Line) error {
 	file, err := os.Open(fw.filename)
 	if err != nil {
 		return err
@@ -27,27 +33,31 @@ func (fw *FileWatcher) Watch(writer chan<- string) error {
 
 	go func() {
 		defer file.Close()
-		var fileMark int64
-		firstRun := true
+		var mark int64
+		first := true
 		for {
-				info, err := file.Stat()
-				if err != nil {
-					log.Fatalf("error getting file size: %v", err)
-				}
-				if firstRun {
-					fileMark = info.Size()
-					firstRun = false
-				}
-				if fileMark < info.Size() {
-					file.Seek(fileMark, 0)
-					scanner := bufio.NewScanner(file)
-					for scanner.Scan() {
-						writer <- scanner.Text()
+			info, err := file.Stat()
+			if err != nil {
+				log.Fatalf("error getting file size: %v", err)
+			}
+			if first {
+				mark = info.Size()
+				first = false
+			}
+			if mark < info.Size() {
+				file.Seek(mark, 0)
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					writer <- &Line{
+						Content:   scanner.Text(),
+						Source:    fw.filename,
+						Timestamp: time.Now(),
 					}
 				}
-				fileMark = info.Size()
-				time.Sleep(6 * time.Millisecond)
 			}
+			mark = info.Size()
+			time.Sleep(6 * time.Millisecond)
+		}
 	}()
 
 	return nil
--- internal/filewatcher/watcher_test.go
+++ internal/filewatcher/watcher_test.go
@@ -5,11 +5,10 @@ import (
 	"os"
 	"testing"
 	"time"
+	
 )
 
 func TestWatcherOnlyReadsNewLines(t *testing.T) {
-	// Create temp file with initial content that should be ignored
-	println("starting tgest")
 	tmpfile, err := os.CreateTemp("", "test*.log")
 	if err != nil {
 		t.Fatalf("Failed to create temp file: %v", err)
@@ -17,7 +16,6 @@ func TestWatcherOnlyReadsNewLines(t *testing.T) {
 	defer os.Remove(tmpfile.Name())
 	defer tmpfile.Close()
 
-	// Write initial content that should be ignored by watcher
 	initialContent := "old line 1\nold line 2\n"
 	if _, err := tmpfile.WriteString(initialContent); err != nil {
 		t.Fatalf("Failed to write initial content: %v", err)
@@ -28,16 +26,17 @@ func TestWatcherOnlyReadsNewLines(t *testing.T) {
 	} else {
 		t.Logf("File size: %d", info.Size())
 	}
-	// Start the watcher
+
 	fw, err := NewFileWatcher(tmpfile.Name())
 	if err != nil {
 		t.Fatalf("Error instantiating file watcher")
 	}
-	lines := make(chan string, 10)
+	lines := make(chan *Line, 10)
 
 	if err := fw.Watch(lines); err != nil {
 		t.Fatalf("Unable to open file to watch")
 	}
+
 	// Give the watcher a moment to start up
 	time.Sleep(300 * time.Millisecond)
 
@@ -46,12 +45,13 @@ func TestWatcherOnlyReadsNewLines(t *testing.T) {
 			t.Fatalf("Failed to append to file: %v", err)
 		}
 	}
+
 	done := make(chan bool)
 
 	go func() {
 		for i := 0; i < 5; i++ {
 			line := <-lines
-			if line != fmt.Sprintf("test line %d", i) {
+			if line.Content != fmt.Sprintf("test line %d", i) {
 				t.Errorf("Expected: %s\nReceived: ", line)
 			}
 		}
